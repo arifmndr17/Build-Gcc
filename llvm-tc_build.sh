eafdaee434aeb3e4c91434f8977ed92f69b1195f
Arif Munandar <90164420+arifmndr17@users.noreply.github.com>
2021-11-05 14:48:34 +0700

    Create llvm-tc_build.sh

---
+++ llvm-tc_build.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
+
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+
+# Set a directory
+export script_dir="$PWD"
+
+# Inlined function to post a message
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+tg_post_build() {
+	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
+	-F chat_id="$TG_CHAT_ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3"
+}
+
+# Build Info
+rel_date="$(date "+%Y%m%d")" # ISO 8601 format
+rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
+builder_commit="$(git rev-parse HEAD)"
+
+# Send a notificaton to TG
+tg_post_msg "<b>Finix GCC: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
+
+echo ::set-output name=date::$(/bin/date -u "+%Y%m%d")
+alias gcc=gcc-10
+alias g++=g++-10
+git config --global user.name "${GITHUB_USER}"
+git config --global user.email "${GITHUB_EMAIL}"
+git clone https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/AnGgIt88/gcc-arm64 ../gcc-arm64 -b gcc-master
+rm -rf ../gcc-arm64/*
+chmod a+x build-*.sh
+tg_post_msg "<b>Building GCC. . .</b>"
+./build-gcc.sh -a arm64
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+tg_post_msg "<b>Building LLD. . .</b>"
+./build-lld.sh -a arm64
+cd ../gcc-arm64
+./bin/aarch64-elf-gcc -v 2>&1 | tee /tmp/gcc-version
+./bin/aarch64-elf-ld.lld -v 2>&1 | tee /tmp/lld-arm64-version
+bash "$script_dir/strip-binaries.sh"
+git add . -f
+git commit -asm "ExtHyper:GCC ARM64 bump to" -m $rel_date -m "Configuration: $(/bin/cat /tmp/gcc-version)" -m "LLD: $(/bin/cat /tmp/lld-arm64-version)"
+git gc
+git push origin gcc-master -f
